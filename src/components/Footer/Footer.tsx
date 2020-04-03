9a7489cc7007d5956e7c4ec819333c2b257f84c3
GoudekettingRM <r.goudeketting@gmail.com>
2020-04-03 10:28:03 +0200

    add linkedIn and Github links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,6 +3,11 @@ import React from "react";
 export const Footer: React.FC = () => {
   return (
     <footer>
+      <hr />
+      <p>
+        <a href="https://github.com/GoudekettingRM">Github</a> &{" "}
+        <a href="https://www.linkedin.com/in/robinmgoudeketting/">LinkedIn</a>
+      </p>
       <h6>© 2020. All rights reserved. Robin Goudeketting.</h6>
     </footer>
   );
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -4,6 +4,9 @@ export const Home: React.FC = () => {
   return (
     <div>
       <h1>Home!</h1>
+      <section>Showcase projects</section>
+      <section>About me + download cv</section>
+      <section>Get in touch</section>
     </div>
   );
 };
