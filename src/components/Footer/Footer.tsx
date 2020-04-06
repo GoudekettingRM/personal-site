8f58ce527093d8915596519bd065e58ad2089d94
GoudekettingRM <r.goudeketting@gmail.com>
2020-04-06 13:27:29 +0200

    add github and linkedin icons

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,26 @@
 import React from "react";
+import Github from "../../icons/github.svg";
+import LinkedIn from "../../icons/linkedin.svg";
 
 export const Footer: React.FC = () => {
   return (
     <footer>
       <hr />
       <p>
-        <a href="https://github.com/GoudekettingRM">Github</a> &{" "}
-        <a href="https://www.linkedin.com/in/robinmgoudeketting/">LinkedIn</a>
+        <a
+          className="externalLink"
+          href="https://github.com/GoudekettingRM"
+          target="_blank"
+          rel="noopener noreferrer">
+          <img className="icon" src={Github} alt="Github logo" />
+        </a>
+        <a
+          className="externalLink"
+          href="https://www.linkedin.com/in/robinmgoudeketting/"
+          target="_blank"
+          rel="noopener noreferrer">
+          <img className="icon" src={LinkedIn} alt="LinkedIn logo" />
+        </a>
       </p>
       <h6>© 2020. All rights reserved. Robin Goudeketting.</h6>
     </footer>
